bc124c2415a56cc8eeb86f3668ef388227c2dea9
Ercan Ozturk <e.ozturk111@gmail.com>
2014-12-20 20:19:12 +0200

    Better looking test methods added

--- test.js
+++ test.js
@@ -1,14 +1,56 @@
+var out = document.getElementById("out");
+
 function addToHistory(item) {
+	if (item == 'apples') {
+		loadApples();	
+	} else if (item == 'pears') {
+		loadPears();
+	} else if (item == 'oranges') {
+		loadOranges();	
+	} else {
+		out.innerHTML = item;		
+	}
+}
+
+function loadApples() {
 	var xmlHttp = new XMLHttpRequest();
 
 	xmlHttp.onreadystatechange = function () {
 		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-			var out = document.getElementById("out");
 			out.innerHTML = xmlHttp.responseText;
-			history_update("addToHistory('" + item + "')");
+			history_update("loadApples()");
 		}	
 	}
 
-	xmlHttp.open("GET", "php/loadItem.php?item=" + item);
+	xmlHttp.open("GET", "php/loadItem.php?item=apples");
 	xmlHttp.send();
 }
+
+function loadPears() {
+	var xmlHttp = new XMLHttpRequest();
+
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			out.innerHTML = xmlHttp.responseText;
+			history_update("loadPears()");
+		}	
+	}
+
+	xmlHttp.open("GET", "php/loadItem.php?item=pears");
+	xmlHttp.send();
+}
+
+function loadOranges() {
+	var xmlHttp = new XMLHttpRequest();
+
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			out.innerHTML = xmlHttp.responseText;
+			history_update("loadOranges()");
+		}	
+	}
+
+	xmlHttp.open("GET", "php/loadItem.php?item=oranges");
+	xmlHttp.send();
+}
+
